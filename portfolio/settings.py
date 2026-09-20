7b1c5b93c97d9de1e961f86bb85424c8e0979507
Moussandao <ndaomoussa07@gmail.com>
2026-09-21 00:13:16 +0100

    Fix WhiteNoise

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -119,7 +119,7 @@ STATICFILES_DIRS = [
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # 3. Moteur WhiteNoise pour servir le CSS/JS compressé sur Render
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
 # ==========================================
